613f9bb63316f5e8477fa19746b9938d3b4e5223
Effi Tramountani <effi.tramountani@gmail.com>
2021-06-22 18:12:05 +0200

    Wrap code in a class.

--- main.py
+++ main.py
@@ -7,8 +7,51 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def normalize(x, current_min, current_max):
-    return (x - current_min) / (current_max - current_min)
+class ONLINE:
+    def __init__(self, stream, drift_detector, predictor):
+        self.window_size = 300
+        self.batch_size = 256
+        self.stream = stream
+        self.drift_detector = drift_detector
+        self.predictor = predictor
+
+        x, y = self.stream.next_sample(self.batch_size)
+        self.current_min = np.min(x, axis=0)
+        self.current_max = np.max(x, axis=0)
+        x = self.normalize(x)
+        self.window_x = x
+        self.window_y = y
+
+        predictor.fit(self.window_x, self.window_y)
+
+    def run(self):
+        ctr_outer = 0
+        while self.stream.has_more_samples():
+            y_pred = self.predictor.predict(self.window_x)
+
+            ctr_inner = 0
+            for i in range(len(self.window_y)):
+                self.drift_detector.add_element(self.window_y[i] == y_pred[i])
+
+                if self.drift_detector.detected_change():
+                    print(f'Change detected at index: {ctr_outer}.{ctr_inner}')
+                    self.current_max = np.max(self.window_x, axis=0)
+                    self.current_min = np.min(self.window_x, axis=0)
+                ctr_inner += 1
+
+            try:
+                x, y = self.stream.next_sample(self.batch_size)
+                x = self.normalize(x)
+            except ValueError:
+                break
+            self.window_x = np.concatenate((self.window_x, x), axis=0)[-self.window_size:]
+            self.window_y = np.concatenate((self.window_y, y))[-self.window_size:]
+            ctr_outer += 1
+
+        self.stream.restart()
+
+    def normalize(self, x):
+        return (x - self.current_min) / (self.current_max - self.current_min)
 
 
 alternate1 = ConceptDriftStream(
@@ -17,7 +60,7 @@ alternate1 = ConceptDriftStream(
     width=1,
     random_state=0)
 
-stream = ConceptDriftStream(
+concept_drift_stream = ConceptDriftStream(
     stream=SEAGenerator(balance_classes=False, classification_function=0, random_state=112, noise_percentage=0.1),
     drift_stream=alternate1,
     position=50000,
@@ -25,52 +68,7 @@ stream = ConceptDriftStream(
     random_state=0)
 
 adwin = ADWIN()
-classifier = Perceptron()
+perceptron = Perceptron()
 
-window_size = 300
-batch_size = 256
-
-x, y = stream.next_sample(batch_size)
-window_x = x
-window_y = y
-current_min = np.min(window_x, axis=0)
-current_max = np.max(window_x, axis=0)
-current_mean = np.mean(window_x, axis=0)
-x = normalize(x, current_min, current_max)
-
-classifier.fit(window_x, window_y)
-ctr = 0
-
-
-while stream.has_more_samples():
-    y_pred = classifier.predict(window_x)
-
-    ctr_2 = 0
-    for i in range(len(y)):
-        adwin.add_element(y[i] == y_pred[i])
-
-        if adwin.detected_change():
-            print(f'Change detected at index: {ctr}.{ctr_2}')
-            current_max = np.max(window_x, axis=0)
-            current_min = np.min(window_x, axis=0)
-            current_mean = np.mean(window_x, axis=0)
-        ctr_2 += 1
-
-    try:
-        x, y = stream.next_sample(batch_size)
-        x = normalize(x, current_min, current_max)
-    except ValueError:
-        break
-    window_x = np.concatenate((window_x, x), axis=0)[-window_size:]
-    window_y = np.concatenate((window_y, y))[-window_size:]
-    ctr += 1
-
-stream.restart()
-
-# def compute_min_max(delta, window_x):
-#     _min = np.min(window_x, axis = 0)
-#     _max = np.max(window_x, axis = 0)
-#
-#     if delta<np.abs(np.mean(window_x, axis=0)-current_mean)/current_mean:
-#         current_min = _min
-#         current_max = _max
+online = ONLINE(concept_drift_stream, adwin, perceptron)
+online.run()
