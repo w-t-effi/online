c729288af0e9500cb3777fd109d85543c6b1cd02
Effi Tramountani <effi.tramountani@gmail.com>
2021-06-24 14:30:04 +0200

    Add kdd_conceptdrift.csv feature names. Update SHAP and FIRES evaluation points. Add predictor update.

--- online.py
+++ online.py
@@ -1,6 +1,7 @@
 import numpy as np
 import shap
 import matplotlib.pyplot as plt
+from utils import get_kdd_conceptdrift_feature_names
 
 
 class ONLINE:
@@ -13,7 +14,7 @@ class ONLINE:
         self.remove_outliers = remove_outliers
         self.window_size = 300
         self.batch_size = 256
-        self.n_frames_initial = 10
+        self.n_frames_explanations = 30
 
         x, y = self.stream.next_sample(self.batch_size)
 
@@ -29,8 +30,8 @@ class ONLINE:
         if self.remove_outliers:
             self.window_x, self.window_y = self.remove_outlier_class_sensitive(self.window_x, self.window_y)
 
-        self.explanations_window_x = self.window_x
-        self.explanations_window_y = self.window_y
+        if self.fires_model:
+            self.window_x = self.run_fires()
 
         self.predictor.fit(self.window_x, self.window_y)
 
@@ -39,10 +40,9 @@ class ONLINE:
         while self.stream.has_more_samples():
             y_pred = self.predictor.predict(self.window_x)
 
-            if ctr_outer == self.n_frames_initial:
-                self.run_fires(ctr_outer) if self.fires_model else self.run_shap(ctr_outer)
-                self.explanations_window_x = self.explanations_window_x[-self.batch_size:]
-                self.explanations_window_y = self.explanations_window_y[-self.batch_size:]
+            if not self.fires_model:
+                if ctr_outer % self.n_frames_explanations == 0 and ctr_outer != 0:
+                    self.run_shap(ctr_outer)
 
             ctr_inner = 0
             for i in range(len(self.window_y)):
@@ -50,13 +50,11 @@ class ONLINE:
 
                 if self.drift_detector.detected_change():
                     print(f'Change detected at index: {ctr_outer}.{ctr_inner}')
-                    self.run_fires(ctr_outer) if self.fires_model else self.run_shap(ctr_outer)
-                    self.explanations_window_x = self.explanations_window_x[-self.batch_size:]
-                    self.explanations_window_y = self.explanations_window_y[-self.batch_size:]
                     self.current_max = np.max(self.window_x, axis=0)
                     self.current_min = np.min(self.window_x, axis=0)
                     self.current_mean = np.mean(self.window_x, axis=0)
                     self.current_std = np.std(self.window_x, axis=0)
+                    break
                 ctr_inner += 1
 
             try:
@@ -67,26 +65,31 @@ class ONLINE:
                 break
             self.window_x = np.concatenate((self.window_x, x), axis=0)[-self.window_size:]
             self.window_y = np.concatenate((self.window_y, y))[-self.window_size:]
-            self.explanations_window_x = np.concatenate((self.explanations_window_x, x), axis=0)
-            self.explanations_window_y = np.concatenate((self.explanations_window_y, y))
             if self.remove_outliers:
                 self.window_x, self.window_y = self.remove_outlier_class_sensitive(self.window_x, self.window_y)
-                self.explanations_window_x, self.explanations_window_y = self.remove_outlier_class_sensitive(self.explanations_window_x, self.explanations_window_y)
+
+            if self.fires_model:
+                self.window_x = self.run_fires()
+
+            self.predictor.partial_fit(self.window_x, self.window_y)
             ctr_outer += 1
 
         self.stream.restart()
 
-    def run_fires(self, time_step):
-        ftr_weights = self.fires_model.weigh_features(self.explanations_window_x, self.explanations_window_y)
+    def run_fires(self):
+        ftr_weights = self.fires_model.weigh_features(self.window_x, self.window_y)
         ftr_selection = np.argsort(ftr_weights)[::-1][:10]
-        x_reduced = np.zeros(self.explanations_window_x.shape)
-        x_reduced[:, ftr_selection] = self.explanations_window_x[:, ftr_selection]
+        # TODO visualize feature weights
+        x_reduced = np.zeros(self.window_x.shape)
+        x_reduced[:, ftr_selection] = self.window_x[:, ftr_selection]
+        return x_reduced
 
     def run_shap(self, time_step):
-        explainer = shap.Explainer(self.predictor, self.explanations_window_x, feature_names=self.stream.feature_names)
-        shap_values = explainer(self.explanations_window_x)
-        shap.plots.beeswarm(shap_values, show=False)
-        plt.savefig(f'test{time_step}.png')
+        explainer = shap.Explainer(self.predictor, self.window_x, feature_names=get_kdd_conceptdrift_feature_names())
+        shap_values = explainer(self.window_x)
+        plt.title(f'Time step: {time_step}, n samples: {self.window_x.shape[0]}')
+        shap.plots.beeswarm(shap_values, plot_size=(20, 10), show=True)
+        # plt.savefig(f'plots/shap{time_step}.png')
 
     def normalize(self, x):
         return (x - self.current_min) / (self.current_max - self.current_min + 10e-99)
--- utils.py
+++ utils.py
@@ -11,3 +11,14 @@ def get_spambase_feature_names():
             "word_freq_edu", "word_freq_table", "word_freq_conference", "char_freq_;", "char_freq_(",
             "char_freq_[", "char_freq_!", "char_freq_$", "char_freq_#", "capital_run_length_average",
             "capital_run_length_longest", "capital_run_length_total"]
+
+
+def get_kdd_conceptdrift_feature_names():
+    return ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment",
+            "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted",
+            "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login",
+            "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate",
+            "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
+            "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
+            "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate",
+            "dst_host_srv_rerror_rate"]
