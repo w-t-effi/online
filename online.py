Fix shap top features computation. Add computation of predictor top features for every run.

online.py
@@ -39,6 +39,7 @@ class ONLINE:
         self.n_frames_explanations = 30
         self.gamma = 0.3
         self.shap_top_features = []
+        self.predictor_top_features = []
         self.accuracy_scores = []
 
         self.n_frames_initial = 10
@@ -130,9 +131,17 @@ class ONLINE:
                 self.window_x = self.run_fires(ctr_outer)
 
             self.predictor.partial_fit(self.window_x, self.window_y)
+            ftr_weights = self.predictor.coef_[0]
+            ftr_selection = np.argsort(ftr_weights)[::-1][:10]
+            self.predictor_top_features.append(ftr_selection)
+
             ctr_outer += 1
 
-        self.draw_top_features_plot()
+        selection = self.fires_model.selection if self.fires_model else self.shap_top_features
+        title = 'FIRES top features' if self.fires_model else 'SHAP top features'
+        path_name = 'fires_top.png' if self.fires_model else 'shap_top.png'
+        self.draw_top_features_plot(selection, title, path_name)
+        self.draw_top_features_plot(self.predictor_top_features, 'Predictor top features', 'predictor_top.png')
         self.draw_accuracy()
         self.stream.restart()
 
@@ -160,11 +169,11 @@ class ONLINE:
             time_step (int): the current time step of the evaluation
         """
         explainer = shap.Explainer(self.predictor, self.window_x, feature_names=get_kdd_conceptdrift_feature_names())
-        ftr_weights = explainer.coef
+        shap_values = explainer(self.window_x)
+        ftr_weights = np.abs(shap_values.values).mean(axis=0)
         ftr_selection = np.argsort(ftr_weights)[::-1][:10]
         self.shap_top_features.append(ftr_selection)
         if time_step % self.n_frames_explanations == 0 and time_step != 0:
-            shap_values = explainer(self.window_x)
             plt.title(f'Time step: {time_step}, n samples: {self.window_x.shape[0]}')
             shap.plots.beeswarm(shap_values, plot_size=(30, 15), show=False)
             plt.savefig(self.dir_path + f'shap{time_step}.png', bbox_inches='tight')
@@ -210,14 +219,13 @@ class ONLINE:
 
         return x[bools == 1]
 
-    def draw_top_features_plot(self):
+    def draw_top_features_plot(self, top_features, title, path_name):
         """
         Draws the most selected features over time.
         """
-        selection = self.fires_model.selection if self.fires_model else self.shap_top_features
         fig, ax = plt.subplots(figsize=(20, 12))
         ax.grid(True, axis='y')
-        y = [feature for features in selection for feature in features]
+        y = [feature for features in top_features for feature in features]
         counts = np.bincount(y)
         top_ftr_idx = counts.argsort()[-10:][::-1]
         ax.bar(np.arange(10), counts[top_ftr_idx], width=0.3, zorder=100)
@@ -227,9 +235,7 @@ class ONLINE:
         ax.tick_params(axis='both', labelsize=20 * 0.7, length=0)
         ax.set_xticks(np.arange(10))
         ax.set_xlim(-0.2, 9.2)
-        title = 'FIRES top features' if self.fires_model else 'SHAP top features'
         plt.title(title, size=20)
-        path_name = 'fires_top.png' if self.fires_model else 'shap_top.png'
         plt.savefig(self.dir_path + path_name, bbox_inches='tight')
         plt.close()
 